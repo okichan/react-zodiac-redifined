c70517c3144d23b4d600049388b22c1ec909460f
Tomomi <madeinwatashi@gmail.com>
2018-03-15 22:01:48 +1100

    Get API ready for deployment

--- api/src/models/init.js
+++ api/src/models/init.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/zodiac-redefined");
+mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
--- api/src/server.js
+++ api/src/server.js
@@ -1,3 +1,8 @@
+// Load .env file in development
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
+
 const express = require("express");
 const cors = require("cors");
 
